602be272dbdfd0d8949d2fd436ada35cbff6b2a2
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2022-04-10 18:39:22 -0500

    css, html and javascript

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
 
-function App() {
+function App() 
+{
+  const [operation, setOperation] = new useState("");
+  const mathOperators = ['%', '/', '*', '-', '+', '+/-', '.'];
+
+  function ClickNumber(number)
+  {
+    setOperation(operation + number);
+  }
+
+  function ClickOperator(operator)
+  {
+    
+    if((operation == "" && operator != '-') || mathOperators.includes(operation.slice(-1)))
+    {
+      return;
+    }
+    setOperation(operation + operator);
+  }
+
+  function UpdateResult()
+  {
+    setOperation(eval(operation).toString());
+  }
+
+  function clearResult()
+  {
+    setOperation("");
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='BodyGrid'>
+        <div className='Result'>
+          <a>{operation || "0"}</a>
+        </div>
+        <button onClick={() => {clearResult()}}>AC</button>
+        <button onClick={() => {ClickOperator("*(-1)")}}>+/-</button>
+        <button onClick={() => {ClickOperator('%')}}>%</button>
+        <button onClick={() => {ClickOperator('/')}} className='MathOperator'>/</button>
+        <button onClick={() => {ClickNumber(7)}}>7</button>
+        <button onClick={() => {ClickNumber(8)}}>8</button>
+        <button onClick={() => {ClickNumber(9)}}>9</button>
+        <button onClick={() => {ClickOperator('*')}} className='MathOperator'>X</button>
+        <button onClick={() => {ClickNumber(4)}}>4</button>
+        <button onClick={() => {ClickNumber(5)}}>5</button>
+        <button onClick={() => {ClickNumber(6)}}>6</button>
+        <button onClick={() => {ClickOperator('-')}} className='MathOperator'>-</button>
+        <button onClick={() => {ClickNumber(1)}}>1</button>
+        <button onClick={() => {ClickNumber(2)}}>2</button>
+        <button onClick={() => {ClickNumber(3)}}>3</button>
+        <button onClick={() => {ClickOperator('+')}} className='MathOperator'>+</button>
+        <button onClick={() => {ClickNumber(0)}} id='cero'>0</button>
+        <button onClick={() => {ClickOperator('.')}}>.</button>
+        <button onClick={() => {UpdateResult()}} className='MathOperator'>=</button>
+      </div>
     </div>
   );
 }
